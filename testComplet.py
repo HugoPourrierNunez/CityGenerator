4a1be45d0d8475ef0548e9fe5ef288b61c70d5fa
anthonybazelle <a.bazelle1@laposte.net>
2016-03-02 18:17:08 +0100

    Maj

--- testComplet.py
+++ testComplet.py
@@ -69,8 +69,8 @@ def run(obj, texture):
     mtex.diffuse_color_factor = 1.0
     mtex.blend_type = 'MULTIPLY'
  
-    # Create new cube and give it UVs
-    obj.mode_set(mode='EDIT')
+    # Assign UVs to object
+    obj.object.mode_set(mode='EDIT')
     obj.uv.smart_project()
     obj.object.mode_set(mode='OBJECT')
  
@@ -81,7 +81,7 @@ def run(obj, texture):
  
     return
 	
-""" ///////////////////// PLAN 2D \\\\\\\\\\\\\\\\\\\\"""
+""" ///////////////////// PLAN 2D \\\\\\\\\\\\\\\\\\\\ """
 
 class Point:
     def __init__(self,x=0,y=0,z=0):
@@ -234,7 +234,7 @@ class View3dPanel():
     bl_category="City Generator"
 
 
-class CityGenerator(View3dPanel,bpy.types.Panel):
+class CityGeneratorPanel(View3dPanel,bpy.types.Panel):
     bl_label="City Generator"   
     
     
@@ -265,73 +265,6 @@ class CityGenerator(View3dPanel,bpy.types.Panel):
         row.operator(operator = "mesh.primitive_monkey_add",text = "Monkey",icon = "MESH_MONKEY")
         row.operator(operator = "mesh.primitive_torus_add",text = "Torus",icon = "MESH_TORUS")
         col.operator(operator = "mesh.primitive_cylinder_add",text = "Cylinder",icon = "MESH_CYLINDER")
-
-
-bpy.types.Scene.myEnumitem=bpy.props.EnumProperty(name ="Obj Type",items=item_type_lst)
-bpy.types.Scene.myEnumitem_obj=bpy.props.EnumProperty(name ="Object",items=item_type_obj)
-
-	
-""" ///////////////////// PANEL \\\\\\\\\\\\\\\\\\\\\ """
-
-def item_type_lst(self,context):
-    types={obj.type:obj.type for obj in context.scene.objects}
-    return [(obj,obj,"")for obj in types]
-
-
-
-def item_type_obj(self,context):
-    return[(obj.name,obj.name,"") for obj in context.scene.objects if obj.type==context.scene.myEnumitem]
-
-class SimpleOperator(bpy.types.Operator):
-    bl_idname="dh.simple_opt"
-    bl_label="Simple Operator"
-    
-    def execute(self,context):
-        bpy.ops.mesh.primitive_cube_add()
-        bpy.ops.mesh.primitive_cone_add()
-        print("Hello WOrld")
-        
-        
-        return{'FINISHED'}
-
-class View3dPanel():    
-    bl_space_type="VIEW_3D"
-    bl_region_type="TOOLS"
-    bl_category="City Generator"
-
-
-class CityGenerator(View3dPanel,bpy.types.Panel):
-    bl_label="City Generator"   
-    
-    
-    def draw(self,context):
-        layout=self.layout
-        layout.operator(operator = "dh.simple_opt",text = "Create City",icon = "RADIO")
-        self.layout.split()
-        self.layout.split()
-        self.layout.split()
-        
-        
-
-        col=layout.column(align = True)
-        sub = col.column(align=True)
-        scene = context.scene
-        rd = scene.render
-        sub.label(text = "// Parameters : \\")
-        sub.label(text = "")
-        sub.label(text = "Dimensions:")
-        sub.prop(rd, "resolution_x", text="X")
-        sub.prop(rd, "resolution_y", text="Y")
-        sub.label(text="")
-        sub.label(text="Streets :")
-        sub.prop(scene, "frame_step")
-        col.operator(operator = "mesh.primitive_cube_add",text = "Cube",icon = "MESH_CUBE")
-        col.operator(operator = "mesh.primitive_cone_add",text = "Cone",icon = "MESH_CONE")
-        row=col.row(align = True)
-        row.operator(operator = "mesh.primitive_monkey_add",text = "Monkey",icon = "MESH_MONKEY")
-        row.operator(operator = "mesh.primitive_torus_add",text = "Torus",icon = "MESH_TORUS")
-        col.operator(operator = "mesh.primitive_cylinder_add",text = "Cylinder",icon = "MESH_CYLINDER")
-
 
 bpy.types.Scene.myEnumitem=bpy.props.EnumProperty(name ="Obj Type",items=item_type_lst)
 bpy.types.Scene.myEnumitem_obj=bpy.props.EnumProperty(name ="Object",items=item_type_obj)
@@ -342,8 +275,9 @@ if __name__=='__main__':
 if __name__ == "__main__":
     m = Map2D()
     m.perform(5)
+    pathTexture = 'C:/Users/User/Documents/GitHub/CityGenerator/Texture/gratteciel.jpg'
     myObj = bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
-    run(myObj, 'C:/Users/User/Documents/GitHub/CityGenerator/Texture/gratteciel.jpg')
+    run(myObj, pathTexture)
 	
 	
 	
